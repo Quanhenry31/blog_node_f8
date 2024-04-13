702997d2a4525788acb37dfa1af260a503dde5f1
quan <tranbaminhquan20003@gmail.com>
2024-04-13 15:08:26 +0700

    delete css

--- src/index.js
+++ src/index.js
@@ -1,27 +1,39 @@
-const path = require('path');
-const express = require('express');
-const morgan = require('morgan');
-const handlebars = require('express-handlebars');
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, "public")));
+
 //HTTP LOGGER
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
 //TEMPLATE ENGINE
-app.engine('hbs', handlebars.engine({
-  extname:'.hbs'
-}));
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+app.get("/news", (req, res) => {
+  res.render("news");
+});
+
+app.get("/", (req, res) => {
+  return res.send(`
+  <h1>Hello work!</h1>
+  `);
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
