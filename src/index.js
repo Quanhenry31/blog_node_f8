d044307e74f108603db4c8759c32a8c69b1a7fd2
quan <tranbaminhquan20003@gmail.com>
2024-04-15 18:42:43 +0700

    route site

--- src/index.js
+++ src/index.js
@@ -5,7 +5,16 @@ const handlebars = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+const route = require("./routes")
+
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json())
+
+//XMLHttpRequest, fetch, axios,
 
 //HTTP LOGGER
 app.use(morgan("combined"));
@@ -19,20 +28,12 @@ app.engine(
 );
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
+//home, search, contact
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
+// route init
+route(app);
 
-app.get("/news", (req, res) => {
-  res.render("news");
-});
 
-app.get("/", (req, res) => {
-  return res.send(`
-  <h1>Hello work!</h1>
-  `);
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
